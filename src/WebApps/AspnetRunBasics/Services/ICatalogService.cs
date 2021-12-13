de93b588a01cbb523c2e9c151a2c86e1e55a1593
Allan Lopez <allankarl.lopez@softvision.com>
2021-12-13 21:41:57 +0800

    refactor

---
+++ src/WebApps/AspnetRunBasics/Services/ICatalogService.cs
@@ -0,0 +1,14 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public interface ICatalogService
+    {
+        Task<CatalogModel> CreateCatalog(CatalogModel model);
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<CatalogModel> GetCatalog(string id);
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+    }
+}
